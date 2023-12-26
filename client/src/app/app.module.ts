7381c7038f951256196ebebb514fcad4d7b1e435
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2023-12-26 12:21:24 -0800

    Added register form

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,9 +9,10 @@ import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeComponent],
+  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
