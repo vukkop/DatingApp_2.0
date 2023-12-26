2ff6fcae6ab6fdd39ddbc01c942b72839592429b
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2023-12-25 22:00:15 -0800

    Persisting th login

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -12,7 +12,16 @@ export class NavComponent implements OnInit {
 
   constructor(private accountService: AccountService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getCurrentUser();
+  }
+
+  getCurrentUser() {
+    this.accountService.currentUser$.subscribe({
+      next: (user) => (this.loggedIn = !!user),
+      error: (error) => console.log(error),
+    });
+  }
 
   login() {
     this.accountService.login(this.model).subscribe({
@@ -25,6 +34,7 @@ export class NavComponent implements OnInit {
   }
 
   logout() {
+    this.accountService.logout();
     this.loggedIn = false;
   }
 }
