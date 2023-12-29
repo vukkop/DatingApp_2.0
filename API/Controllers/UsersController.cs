27066e2bf6ba31a0c93e3ddee6ac8b7283356ebb
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2023-12-29 15:57:23 -0800

    Setting new profile photo and added photo to the nav-bar

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -82,5 +82,28 @@ namespace API.Controllers
       return BadRequest("Problem adding photo");
     }
 
+    [HttpPut("set-main-photo/{photoId}")]
+    public async Task<ActionResult> SetMainPhoto(int photoId)
+    {
+      var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var user = await _userRepository.GetUserByUsernameAsync(username);
+
+      if (user == null) return NotFound();
+
+      var photo = user.Photos.FirstOrDefault(x => x.Id == photoId);
+
+      if (photo == null) return NotFound();
+
+      if (photo.IsMain) return BadRequest("This is already your main photo");
+
+      var currentMain = user.Photos.FirstOrDefault(x => x.IsMain);
+      if (currentMain != null) currentMain.IsMain = false;
+      photo.IsMain = true;
+
+      if (await _userRepository.SaveAllAsync()) return NoContent();
+
+      return BadRequest("Problem setting main photo");
+    }
+
   }
 }
