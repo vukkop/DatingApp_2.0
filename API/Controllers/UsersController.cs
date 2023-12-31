b2934c539d352437d171cf0677a0ea8985cd1955
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2023-12-30 22:21:51 -0800

    Added filtering by age and gender server side

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -27,6 +27,15 @@ namespace API.Controllers
     [HttpGet]
     public async Task<ActionResult<PagedList<MemberDto>>> GetUsers([FromQuery] UserParams userParams)
     {
+      var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userRepository.GetUserByUsernameAsync(username);
+      userParams.CurrentUsername = currentUser.UserName;
+
+      if (string.IsNullOrEmpty(userParams.Gender))
+      {
+        userParams.Gender = currentUser.Gender == "male" ? "female" : "male";
+      }
+
       var users = await _userRepository.GetMembersAsync(userParams);
 
       Response.AddPaginationHeader(new PaginationHeader(users.CurrentPage, users.PageSize, users.TotalCount, users.TotalPages));
